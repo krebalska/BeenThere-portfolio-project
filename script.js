2097a84b0af024e029bef3ebc693b4ded9a87341
krebalska <krbalska@interia.pl>
2022-12-13 18:07:04 +0100

    improve homepage design deploy pin bubbles yearly copyrights update nav underline animation

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const heroContainer = document.querySelector(".hero-container");
 const infoContainer = document.querySelector(".info-container");
 const tryContainer = document.querySelector(".try-container");
 const testimonialContainer = document.querySelector(".testimonial-container");
+///////////////////////////
+//updating year of copyrights
+const yearEl = document.querySelector(".copyrights-update");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = `${currentYear} Klaudia Rębalska`;
 
 // //shining sign-up button
 
@@ -54,3 +60,111 @@ document.addEventListener("keydown", function (e) {
     }
   }
 });
+
+///////////bubble comments
+const black = document.querySelector(".black");
+const red = document.querySelector(".red");
+const purple = document.querySelector(".purple");
+const yellow = document.querySelector(".yellow");
+const orange = document.querySelector(".orange");
+const green = document.querySelector(".green");
+const blue = document.querySelector(".blue");
+const white = document.querySelector(".white");
+const bubble = document.querySelector(".bubble");
+
+const bubbleColorComment = {
+  black() {
+    const html = `<div class="bubble-black">
+        <div class="info-comment left" contenteditable="false">
+        Use <b>black pin</b> to mark your dream destinations
+        </div>
+        </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  red() {
+    const html = `<div class="bubble-red">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>red pin</b> to mark your romantic memories
+  </div>
+  </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  purple() {
+    const html = `<div class="bubble-purple">
+    <div class="info-comment left" contenteditable="false">
+    Use <b>purple pin</b> to mark your destinations that you will be soon
+    </div>
+    </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  yellow() {
+    const html = `<div class="bubble-yellow">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>yellow pin</b> to mark your family memories
+  </div>
+  </div>`;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  orange() {
+    const html = `<div class="bubble-orange">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>orange pin</b> to mark your memories with your friends
+  </div>
+  </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  green() {
+    const html = `<div class="bubble-green">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>green pin</b> to mark your long trips
+  </div>
+  </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  blue() {
+    const html = `<div class="bubble-blue">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>blue pin</b> to mark your best adventures
+  </div>
+  </div> `;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+  white() {
+    const html = `<div class="bubble-white">
+  <div class="info-comment left" contenteditable="false">
+  Use <b>white pin</b> to mark your other adventures
+  </div>
+  </div>`;
+    bubble.insertAdjacentHTML("afterbegin", html);
+  },
+
+  // addBubleColor(color = this.colors) {
+  //   const html = color;
+  //   console.log(html);
+  //   infoContainer.insertAdjacentHTML("afterbegin", html);
+  // },
+};
+
+const removeComment = function () {
+  if (bubble) {
+    bubble.innerHTML = "";
+    infoContainer.parentNode.removeChild(bubble);
+  }
+};
+
+black.addEventListener("mouseenter", bubbleColorComment.black);
+black.addEventListener("mouseleave", removeComment);
+red.addEventListener("mouseenter", bubbleColorComment.red);
+red.addEventListener("mouseleave", removeComment);
+purple.addEventListener("mouseenter", bubbleColorComment.purple);
+purple.addEventListener("mouseleave", removeComment);
+yellow.addEventListener("mouseenter", bubbleColorComment.yellow);
+yellow.addEventListener("mouseleave", removeComment);
+orange.addEventListener("mouseenter", bubbleColorComment.orange);
+orange.addEventListener("mouseleave", removeComment);
+green.addEventListener("mouseenter", bubbleColorComment.green);
+green.addEventListener("mouseleave", removeComment);
+blue.addEventListener("mouseenter", bubbleColorComment.blue);
+blue.addEventListener("mouseleave", removeComment);
+white.addEventListener("mouseenter", bubbleColorComment.white);
+white.addEventListener("mouseleave", removeComment);
